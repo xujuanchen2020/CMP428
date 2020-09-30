3315460f8e38a0bcf1dc3dbbd8a458f2f7841ee4
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-09-30 16:09:03 -0400

    Create Rect.java

---
+++ week 3/Rect.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+
+public class Rect {
+	int x;
+	int y;
+	
+	int w;
+	int h;
+	
+	public Rect(int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	
+	public void moveBy(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	public void draw(Graphics g) {
+		g.drawRect(x, y, w, h);
+	}
+
+}
