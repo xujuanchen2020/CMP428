95adebd689d0e2cdbe5515deaedc83d63b795a06
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-10-11 18:25:39 -0400

    Create Tank.java

---
+++ Week_5/AABB/Tank.java
@@ -0,0 +1,56 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Tank extends PolygonModel2D{
+	
+	Rect AABB ;
+	
+	public static int[][] tank_x_structure = {
+		{ -25, 25, 25, -25 },
+		{ -10, 10, 10, -10 },
+		{ -3, 3, 3, -3 },
+		{ -35, -25, -25, -35 },
+		{ 35, 25, 25, 35 },
+//		{4,20,30,5},
+//		{4,20,40,5},
+//		{-4,-20,-30,-5},
+//		{-4,-20,-30,-5}
+	};
+	
+	public static int[][] tank_y_structure = {
+		{ -40, -40, 40, 40 },
+		{ -10, -10, 10, 10 },
+		{ -35, -35, -10, -10 },
+		{ -35, -35, 35, 35 },
+		{ -35, -35, 35, 35 },
+//		{3,3,70,50},
+//		{-3,-3,-70,-50},
+//		{3,3,70,50},
+//		{-3,-3,-70,-50}
+	};
+
+	public Tank (int x, int y, int A) {
+		super(x,y,A,tank_x_structure,tank_y_structure);
+		
+		AABB = new Rect(x-40,y-40,80,80);
+	}
+	
+	
+	public void moveBy(int dx, int dy) {
+		super.moveBy(dx, dy);
+		
+		AABB.moveBy(dx, dy);
+	}
+	
+	public boolean overlaps(Tank tank) {
+		return AABB.overlaps(tank.AABB);
+	}
+	
+	public void draw(Graphics g) {
+		super.draw(g);
+		
+		AABB.draw(g);
+	}
+	
+}
