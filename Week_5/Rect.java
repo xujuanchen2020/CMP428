e01e3f21d3b966c95f4e851658a15d656bde79fd
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-10-10 22:04:30 -0400

    Create Rect.java

---
+++ Week_5/Rect.java
@@ -0,0 +1,40 @@
+import java.awt.Graphics;
+
+public class Rect {
+	int x;
+	int y;
+	
+	int w;
+	int h;
+	
+	public Rect(int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	
+	public boolean overlaps(Rect r) {
+		return  (r.x + r.w >= x  )&&
+				(x + w     >= r.x)&&
+				(r.y + r.h >= y  )&&
+				(y + h     >= r.y);
+	}
+	
+	public boolean contains(int mx, int my) {
+		return  (mx > x) &&
+				(mx < x + w) &&
+				(my > y) &&
+				(my < y + h);
+	}
+	
+	public void moveBy(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	public void draw(Graphics g) {
+		g.drawRect(x, y, w, h);
+	}
+
+}
